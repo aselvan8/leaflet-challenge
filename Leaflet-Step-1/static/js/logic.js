db222998d8849da8cdf41efecff11a0688ef3ece
aselvan8 <65464009+aselvan8@users.noreply.github.com>
2020-09-16 22:30:41 -0400

    js changes

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -4,7 +4,7 @@ var queryurl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 d3.json(queryurl, function(data) {
     // Once we get a response, send the data.features object to the createFeatures function
     createFeatures(data.features);
-  });
+});
 
 function createFeatures(earthquakeData) {
 
@@ -39,18 +39,17 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
     // Define streetmap layers
-    var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-        attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-        tileSize: 512,
+    // Define variables for our tile layers
+    var light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
         maxZoom: 18,
-        zoomOffset: -1,
-        id: "mapbox/streets-v11",
+        id: "light-v10",
         accessToken: API_KEY
     });
 
     // Define a baseMaps object to hold our base layers
     var baseMaps = {
-        "Street Map": streetmap
+        "Light Map": light
     };
 
     // Create overlay object to hold our overlay layer
@@ -64,7 +63,7 @@ function createMap(earthquakes) {
             37.09, -95.71
         ],
         zoom: 5,
-        layers: [streetmap, earthquakes]
+        layers: [light, earthquakes]
     });
 
     // Add the layer control to the map
@@ -84,14 +83,21 @@ function createMap(earthquakes) {
         // loop through our density intervals and generate a label with a colored square for each interval
         for (var i = 0; i < grades.length; i++) {
             div.innerHTML +=
-                '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+                '<i style="background-color:' + getcolor(grades[i] + 1) + ';">'+
+                grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+')+'</i>';
         }
     
         return div;
     };
     
-    legend.addTo(map);
+    legend.addTo(myMap);
+
+    // //Overlay listener for adding
+    // myMap.on('overlayadd', function(a) {
+    //     //Add the legend
+    //     legend.addTo(myMap);
+    // });
+
 }
 
 // function for color
@@ -115,4 +121,3 @@ function getcolor(magnitude) {
 function getradius(magnitude) {
     return magnitude * 14500;
 };
-
